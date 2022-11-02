8c13511f8aa20d8d2df876152262f72b9a650be4
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2022-11-02 11:29:05 +0200

    Lab main

---
+++ Lab_8.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	long long a = 1000;
+	long long *b = &a;
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+
+	unsigned char* pointer = (unsigned char*)b;
+
+	std::cout << &pointer << std::endl;
+
+	for (int i = 0; i < sizeof(long long); i++)
+	{
+		std::cout << (int)*(pointer + i) << " ";
+	}
+}
